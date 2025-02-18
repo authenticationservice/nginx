eada92746924ae0613c918884c3e5fa70051a550
authenticationservice <144920677+authenticationservice@users.noreply.github.com>
2025-02-18 12:43:23 -0600

    Update you.js

--- site/you.js
+++ site/you.js
@@ -1,47 +1,15 @@
 function openWindow(url) {
-    var newWin = window.open(url, "_blank", 'menubar=no, status=no, toolbar=no, resizable=no, width=158, height=100, titlebar=no, alwaysRaised=yes');
+    var newWin = window.open(url, "_blank", 'menubar=no, status=no, toolbar=no, resizable=no, width=200, height=180, titlebar=no, alwaysRaised=yes');
 
     // Move the new window to a random location once
     if (newWin) {
-        var screenWidth = screen.width - 158; // Keep within bounds
-        var screenHeight = screen.height - 100;
+        var screenWidth = screen.width - 200; // Keep within bounds
+        var screenHeight = screen.height - 180;
 
         var randomX = Math.floor(Math.random() * screenWidth);
         var randomY = Math.floor(Math.random() * screenHeight);
 
         newWin.moveTo(randomX, randomY);
-
-        // Write custom content inside the blank page
-        /*newWin.document.write(`
-            <!DOCTYPE html>
-                <html>
-                    <head>
-                        <title>YOU ARE AN IDIOT</title>
-                            <style>
-                                body {
-                                    height: 100vh;
-                                    margin: 0;
-                                }
-                            </style>
-                            <script>
-                                function getRandomColor() {
-                                    const letters = '0123456789ABCDEF';
-                                    let color = '#';
-                                    for (let i = 0; i < 6; i++) {
-                                        color += letters[Math.floor(Math.random() * 16)];
-                                    }
-                                    return color;
-                                }
-                        
-                                document.addEventListener('DOMContentLoaded', function() {
-                                    document.body.style.backgroundColor = getRandomColor();
-                                });
-                            </script>
-                        </head>
-                        <body>
-                    </body>
-            </html>
-        `);*/
     }
 
     return newWin;
